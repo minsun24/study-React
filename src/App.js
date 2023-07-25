a226d30f9506f4ab423dce45af9591cb5951ec44
minsun24 <jms49526@gmail.com>
2023-07-26 02:40:42 +0900

    MUI 공부

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import {Routes, Route, Link}  from "react-router-dom";  
+import Button from '@mui/material/Button';
+import '@fontsource/roboto/300.css'; //roboto 글꼴 사용
+import DeleteIcon from '@mui/icons-material/Delete';  //삭제 쓰레기통 아이콘
+import SendIcon from '@mui/icons-material/Send'; //send 아이콘
 
 import Home from "./pages/Home"; 
 import About from "./pages/About"; 
@@ -15,6 +19,12 @@ function App() {
   return (
     <div className="App">
       <nav>
+      <Button href="/" variant="text" color="primary">home</Button> 
+      <Button href="/about" variant="contained" color="primary">about</Button> 
+      <Button href="/counter" variant="outlined" color="primary">counter</Button> 
+      <Button href="/input" variant="text" color="primary">input</Button> 
+      <Button href="/input2" variant="contained" color="primary">input2</Button> 
+      <Button href="/list" variant="outlined" color="primary">user list</Button> 
         <Link to="/">Home</Link> |
         <Link to="/about">About</Link> |
         <Link to="/counter">Counter</Link> |
@@ -30,9 +40,18 @@ function App() {
         <Route path="/input2" element={<Input2/>}></Route>
         <Route path="/list" element={<Userlist/>}></Route>
       </Routes>
-      
+
+      <Button variant="outlined" startIcon={<DeleteIcon />}>
+        Delete
+      </Button>
+      <Button variant="contained" endIcon={<SendIcon />}>
+        Send
+      </Button>
+ 
     </div>
   );
 }
+
+
 
 export default App;
